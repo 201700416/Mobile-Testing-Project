0907bd2957d38c9fcbd74e3a63ee04e41651f2cb
Mohammed Ali Jaafar <201700416@student.polytechnic.bh>
2020-11-15 07:59:23 +0300

    Initial Commit

---
+++ Mobile Testing Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mobile Testing Project
+//
+//  Created by mobileProg on 11/15/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
